f511c76622570a1397843251126722e5a4cf296f
MNlegion <rkreuser30@gmail.com>
2023-12-26 19:59:27 -0500

    playing around with objects

---
+++ main.js
@@ -0,0 +1,13 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 20,
+    address: {
+        street: '123 Main St',
+        city: 'Boston',
+        state: 'MA'
+    },
+    hobbies: ['reading', 'music', 'movies'],
+}
+
+console.log(person.hobbies[1]);
